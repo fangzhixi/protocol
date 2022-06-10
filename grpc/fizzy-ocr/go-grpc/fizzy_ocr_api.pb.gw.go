0cfb456c48f78716d4cb6d41581634c29f6b3002
unknown <951003864@qq.com>
2022-06-10 22:50:07 +0800

    （zhixi.fang|fizzy-ocr）新增居民身份证识别定义

--- grpc/fizzy-ocr/go-grpc/fizzy_ocr_api.pb.gw.go
+++ grpc/fizzy-ocr/go-grpc/fizzy_ocr_api.pb.gw.go
@@ -67,6 +67,40 @@ func local_request_FizzyOcrApi_GeneralPrintOCRV1_0(ctx context.Context, marshale
 
 }
 
+func request_FizzyOcrApi_ResidentIdentityCardOCRV1_0(ctx context.Context, marshaler runtime.Marshaler, client FizzyOcrApiClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CommonOCRReq
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.ResidentIdentityCardOCRV1(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_FizzyOcrApi_ResidentIdentityCardOCRV1_0(ctx context.Context, marshaler runtime.Marshaler, server FizzyOcrApiServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CommonOCRReq
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.ResidentIdentityCardOCRV1(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterFizzyOcrApiHandlerServer registers the http handlers for service FizzyOcrApi to "mux".
 // UnaryRPC     :call FizzyOcrApiServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -93,6 +127,29 @@ func RegisterFizzyOcrApiHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		}
 
 		forward_FizzyOcrApi_GeneralPrintOCRV1_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_FizzyOcrApi_ResidentIdentityCardOCRV1_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_FizzyOcrApi_ResidentIdentityCardOCRV1_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_FizzyOcrApi_ResidentIdentityCardOCRV1_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -157,13 +214,37 @@ func RegisterFizzyOcrApiHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_FizzyOcrApi_ResidentIdentityCardOCRV1_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_FizzyOcrApi_ResidentIdentityCardOCRV1_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_FizzyOcrApi_ResidentIdentityCardOCRV1_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_FizzyOcrApi_GeneralPrintOCRV1_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"ocr-api", "print", "general", "v1"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_FizzyOcrApi_ResidentIdentityCardOCRV1_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"ocr-api", "credentials", "identity-card", "v1"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
 	forward_FizzyOcrApi_GeneralPrintOCRV1_0 = runtime.ForwardResponseMessage
+
+	forward_FizzyOcrApi_ResidentIdentityCardOCRV1_0 = runtime.ForwardResponseMessage
 )
